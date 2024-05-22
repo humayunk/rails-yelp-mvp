0120214ec705eff6f3dc5d7bc40b2dc7a570f1ea
humayun khan <humayun.n.k@gmail.com>
2024-05-22 15:00:39 -0400

    created models and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+
+Restaurant.destroy_all
+
+restaurants = [
+  { name: "Sushi Place", address: "123 Sushi St, Tokyo, Japan", phone_number: "123-456-7890", category: "Japanese" },
+  { name: "Pizza Palace", address: "456 Pizza Ave, Rome, Italy", phone_number: "098-765-4321", category: "Italian" },
+  { name: "Noodle Bonanza", address: "789 Burger Blvd, New York, USA", phone_number: "555-555-5555", category: "chinese" },
+  { name: "French Corner", address: "321 Curry Rd, New Delhi, India", phone_number: "444-444-4444", category: "french" },
+  { name: "Waffle Tower", address: "654 Taco Ln, Mexico City, Mexico", phone_number: "333-333-3333", category: "belgian" }
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create!(restaurant)
+end
+
+puts "Seeded #{Restaurant.count} restaurants!"
